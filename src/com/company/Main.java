c4c9b0aebf2cc7aba35af469ec5bee5dd6a2488e
eldegen <eliyaz2761@gmail.com>
2021-08-29 20:50:29 +0600

    Выполнено задание на сообразительность

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void canGoOutside(int age, int temp) {
@@ -32,5 +34,26 @@ public class Main {
         canGoOutside(18, 7);
         canGoOutside(46, 23);
         canGoOutside(63, -10);
+        generateRandomAge();
     }
+
+    // Задание на сообразительность
+
+    public static void generateRandomAge() {
+        Random random = new Random();
+        int minAge = 1;
+        int maxAge = 60;
+        int randAge = minAge + (int) (Math.random() * ((maxAge - minAge) + 1));
+
+        int minTemp = -25;
+        int maxTemp = 40;
+        int randTemp = minTemp + (int) (Math.random() * ((maxTemp - minTemp) + 1));
+
+        System.out.println("age random: " + randAge);
+        System.out.println("temp random: " + randTemp);
+
+        System.out.println("Можно ли гулять?: (если строка ниже пуста, значит нет)");
+        canGoOutside(randAge, randTemp);
+    }
+
 }
