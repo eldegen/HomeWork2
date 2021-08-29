09d7bbe69128d3a2602c3d7ed4a30f08d5db0c99
eldegen <eliyaz2761@gmail.com>
2021-08-29 20:05:30 +0600

    Основное задание выполнено

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,35 @@ package com.company;
 
 public class Main {
 
+    public static void canGoOutside(int age, int temp) {
+        if (age >= 20 && age <= 45) {
+            if (temp >= -20 && temp <= 30) {
+
+                System.out.println("Можно идти гулять (1 body)");
+            }
+        }
+
+        if (age > 0 && age < 20) {
+            if (temp >= 0 && temp <= 28) {
+
+                System.out.println("Можно идти гулять (2 body)");
+            }
+        }
+
+        if (age > 45) {
+            if (temp >= -10 && temp <= 25) {
+
+                System.out.println("Можно идти гулять (3 body)");
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        canGoOutside(31, -11);
+        canGoOutside(25, 30);
+        canGoOutside(18, 7);
+        canGoOutside(46, 23);
+        canGoOutside(63, -10);
     }
 }
